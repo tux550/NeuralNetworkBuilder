031aefb000a0c1a4aa1a8042b06e5c1b8cbb912a
tux550 <rodrigosalad@gmail.com>
2023-06-05 21:43:12 -0500

    python test

--- Test/main.py
+++ Test/main.py
@@ -1,7 +1,16 @@
 
 import subprocess
+import numpy as np
+from sklearn.metrics import confusion_matrix
 
-def run_test(parameters_dict):
+def numpy_from_file(filename):
+    return np.genfromtxt(filename, delimiter=' ')
+
+def numpy_from_str(txt):
+    return np.array([[float(e) for e in row.strip(" ").split(" ")] for row in txt.split("\n")])
+
+def run_exe(parameters_dict):
+    # CONSTANTS
     KEY_ORDER = [
         "input_size",
         "output_size",
@@ -17,13 +26,40 @@ def run_test(parameters_dict):
     ]
     EXECUTABLE = "../NeuralNetwork/mlp.exe"
     CMD = 'echo "{parameters}" | {exe_file}'
-
+    # FORMAT
     parameters = " ".join([ str(parameters_dict[k]) for k in KEY_ORDER])
     cmd = CMD.format(parameters=parameters, exe_file=EXECUTABLE)
-
+    # RUN
     print("Running with parameters:", parameters)
-    result=subprocess.getoutput(cmd)
-    return result
+    y_pred =subprocess.getoutput(cmd)
+    y_pred =numpy_from_str(y_pred)
+    return y_pred
+
+
+def metrics(parameters_dict):
+    # Y PRED
+    y_pred = run_exe(parameters_dict)
+    y_pred = np.argmax(y_pred, axis=1)
+    # Y TRUE
+    y_true = numpy_from_file(parameters_dict["filename_y"])
+    y_true = np.argmax(y_true, axis=1)
+    # Generate Matrix
+    cm = confusion_matrix(y_true, y_pred)
+    print("CONFUSION MATRIX")
+    print(cm)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -44,6 +80,4 @@ default = {
 }
 
 
-res = run_test(default)
-print("RESULT:")
-print(res)
+metrics(default)
