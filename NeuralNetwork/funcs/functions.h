78e6a149258b9b59b2319c272aff3bbd1e0884d1
Rodrigo Salazar <rodrigosalad@gmail.com>
2023-06-06 17:59:23 -0500

    functor template

--- NeuralNetwork/funcs/functions.h
+++ NeuralNetwork/funcs/functions.h
@@ -11,5 +11,14 @@ alg::t_type hypertan_drv(alg::t_type x);
 alg::Matrix mse(alg::Matrix &a, alg::Matrix &b);
 alg::Matrix mse_drv(alg::Matrix &a, alg::Matrix &b);
 
+class ActivationFunctor {
+    alg::t_t2t act_fun;
+    alg::t_t2t act_drv;
+};
+
+class LossFunctor {
+    alg::t_mm2m loss_fun;
+    alg::t_mm2m loss_drv;
+};
 
 #endif
