60973b7445f751c836f79c80e80552d79ecb2955
tux550 <rodrigosalad@gmail.com>
2023-06-19 03:22:46 -0500

    96.8% balanced accuracy

--- Test/config_ht.py
+++ Test/config_ht.py
@@ -2,15 +2,17 @@
 default_arch_ht = {
     # ARCHITECTURE
     "input_size": 128,
-    "nodes_size": [100,100,24],
-    "activation_function":["hypertan","hypertan","hypertan"],
+    "nodes_size": [60,60,60,60,60,24],
+    "activation_function":["relu","relu","relu","relu","relu","hypertan"],
+    #"nodes_size": [100,100,24],
+    #"activation_function":["hypertan","hypertan","hypertan"],
     "loss_function":"mse",
 }
 default_train_ht = {
     # TRAIN
     "filename_x_train":"dataset/x_train.csv",
     "filename_y_train":"dataset/y_train.csv",
-    "epochs":10,
+    "epochs":50,
     "alpha":0.01,
 }
 
